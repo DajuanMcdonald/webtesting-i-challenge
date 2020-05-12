0b86101cab9e3aaedb9e7d0b70158e8c7ce15c46
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-12 14:33:55 -0400

    refactor test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,6 +1,7 @@
 const enhancer = require('./enhancer.js');
 // test away!
-describe('testing the enhancer file', () => {
+
+describe('* testing the enhancer file', () => {
     describe('testing the get function', () => {
         it('expect object to be defined', () => {
             expect(enhancer.get({name: 'Sword', durability: 50, enhancement: 10})).toBeDefined()
@@ -9,37 +10,39 @@ describe('testing the enhancer file', () => {
         it('expect object to have property value of', () => {
             expect(enhancer.get({name: 'Bat', durability: 4, enhancement: 2})).toHaveProperty('enhancement', 2)
         })
-    })
+    })    
+})
 
-
-    describe('testing custom matchers for enhancement and durability', () => {
-        describe('item enhancement number range', () => {
-            
-                expect.extend({
-                    toBeInRange(received, floor, ceiling) {
-                        const pass = received >= floor && received <=ceiling;
-                        if(pass) {
-                            return {
-                                message: () => `expected ${received} not to be in range ${floor} - ${ceiling}`,
-                                pass: true,
-                            };
-                        } else {
-                            return {
-                                message: () => `expected ${received} to be in range ${floor} - ${ceiling}`,
-                                pass: false,
-                            };
-                        }
-                    }
-                })
-                it('expect object to be in range of', () => {
-                    const enc = {
-                        enhancement: 20,
-                        durability: 90,
-                    }
-                    expect(enc.enhancement).toBeInRange(0, 20)
-                    expect(enc.durability).toBeInRange(0, 100)
-                })
+//custom toBeInRange matcher
+expect.extend({
+    toBeInRange(received, floor, ceiling) {
+        const pass = received >= floor && received <=ceiling;
+            if(pass) {
+                return {
+                    message: () => `expected ${received} not to be in range ${floor} - ${ceiling}`,
+                        pass: true,
+                        };
+            } else {
+                return {
+                     message: () => `expected ${received} to be in range ${floor} - ${ceiling}`,
+                            pass: false,
+                    };
+                }
+    }
+});
+    
+describe('* testing custom matchers for enhancement and durability', () => {
+    describe('item enhancement number range', () => {
+        it('expect object to be in range of 0-20', () => {
+            const enc = { enhancement: 20 }
+            expect(enc.enhancement).toBeInRange(0, 20)   
         })
     })
     
+    describe('item durability number range', () => {
+        it('expect object to be in range of 0-100', () => {
+            const dur = { durability: 90 }
+            expect(dur.durability).toBeInRange(0, 100)
+        })
+    })
 })
