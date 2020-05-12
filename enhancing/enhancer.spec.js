7b4ada35f6de085ba7a34b0463bb39f979622c52
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-12 07:39:28 -0400

    two testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('testing the enhancer file', () => {
+    describe('testing the get function', () => {
+        it('expect object to be defined', () => {
+            expect(enhancer.get({name: 'Sword', durability: 50, enhancement: 10})).toBeDefined()
+        })
+
+        it('expect object to have property value of', () => {
+            expect(enhancer.get({name: 'Bat', durability: 4, enhancement: 2})).toHaveProperty('enhancement', 2)
+        })
+    })
+})
